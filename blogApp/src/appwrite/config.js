b66abd4a974633de2bb2290f754ab01d7dc911eb
Mridulaoc <112925003+Mridulaoc@users.noreply.github.com>
2023-10-25 22:48:15 +0530

    Update config.js

--- blogApp/src/appwrite/config.js
+++ blogApp/src/appwrite/config.js
@@ -17,7 +17,16 @@ export class DbServices{
     async createPost({title,slug,featuredImage,status,content,username}){
         try {
             return await this.databases.createDocument(
-                conf.appwriteDatabaseId
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    featuredImage,
+                    status,
+                    username,
+                    content,
+                }
             )
         } catch (error) {
 
@@ -25,6 +34,118 @@ export class DbServices{
             
         }
     }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+            return true;
+        } catch (error) {
+           console.log(error)
+            return false;
+        }
+    }
+
+    async updatePost(slug,{title,featuredImage,status,content}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    featuredImage,
+                    status,
+                    content,
+                }
+            )
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    async getPost(slug){
+       try {
+        await this.databases.getDocument(
+            conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+        )
+        return true;
+       } catch (error) {
+        console.log(error);
+        return false;
+       }
+    }
+
+
+    async getPosts(queries=[Query.equal("status","active")]){
+
+        try {
+            await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+            return true;
+        } catch (error) {
+            console.log(error)
+            return false;
+            
+        }     
+
+    }
+
+    async upload(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file,
+            )
+
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+            conf.appwriteBucketId,
+            fileId,
+    
+            )
+            return true;
+        } catch (error) {
+            console.log(error)
+            return false;
+            
+        }
+       
+    }
+
+    getFilePreview(fileId){
+        try {
+            this.bucket.getFilePreview(
+                conf.appwriteBucketId,
+                fileId,
+            )
+        } catch (error) {
+            console.log(error)
+            
+        }
+    }
+
+
+
 
 
 }
