9956da153244f93692f6dfdec5e47bc439ba72cb
Mridulaoc <112925003+Mridulaoc@users.noreply.github.com>
2023-10-26 20:11:49 +0530

    blog app

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -1,15 +1,43 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Header,Footer } from './components/index'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import {logIn,logOut} from './store/authSlicer'
 
 function App() {
- 
 
-  return (
+  const [loading,setLoading]= useState(true);
+  const dispatch =useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userdata)=>{
+      if(userdata){
+        dispatch(logIn({userdata}))
+      }
+      else{
+        dispatch(logOut())
+      }
+    })
+      .finally(()=>setLoading(false))
+    }
+  ,[])
+  
+
+
+
+  return !loading ? (
     <>
+      <div className='h-screen'>
+      <Header/>     
       <h1>Blog App</h1>
+      <Footer/>
+      </div>
      
     </>
-  )
+  ):null
 }
 
 export default App
---
+++ blogApp/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import {Container} from '../index'
+
+const Footer = () => {
+  return (
+    <Container>
+      <footer className='flex justify-between border-t-2 border-gray-400 w-full'>
+        <p>Copy right @ 2023 - Mridula O C</p>
+        <div>Social Icons</div>
+      </footer>
+    </Container>
+  )
+}
+
+export default Footer
