4d1401200627b1e80122c26b596cb69edc51d6a5
Mridulaoc <112925003+Mridulaoc@users.noreply.github.com>
2023-11-04 12:25:06 +0530

    blogApp

---
+++ blogApp/src/components/RTE.jsx
@@ -0,0 +1,39 @@
+
+import {Controller} from 'react-hook-form'
+import {Editor} from '@tinymce/tinymce-react'
+
+export default function RTE ({name,control,defaultValue="",label}) {
+
+  return (
+   <div className='w-full'>
+    {label && <label>{label}</label>}
+    <Controller
+     name={name || "content"}
+     control={control}
+     render={({field:{onchange}})=>(
+        <Editor
+        initialValue={defaultValue}
+        init = {{
+            height:500,
+            menubar:true,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'
+              ],
+            toolbar: 'undo redo | formatselect | ' +
+              'bold italic backcolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+            content_style : 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+
+        onEditorChange={onchange}
+        />
+     )}
+    />
+   </div>
+  )
+}
+
+
